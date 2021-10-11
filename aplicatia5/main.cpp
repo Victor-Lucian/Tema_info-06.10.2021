228f7e19ca305af0af6760b32b90d52a2c918e8f
Victor-Lucian <91419028+Victor-Lucian@users.noreply.github.com>
2021-10-11 12:33:58 +0300

    Modificare cod cu codul din clasa

--- aplicatia5/main.cpp
+++ aplicatia5/main.cpp
@@ -1,30 +1,56 @@
 #include <iostream>
 #include <fstream>
 
+///5. Sa se calculeze minimul dintre valorile care incep cu o cifra para de pe fiecare coloana situata sub diag
+///principala.
+
 using namespace std;
 
-/**
-    Sa se calculeze minimul dintre valorile care incep cu o cifra para
-    de pe fiecare coloana situata sub diag principala.
-*/
+int a[105][105], n;
 
-void citire (int a[105][105], int &n)
+void citire()
 {
     ifstream fin ("matrice.in");
     fin >> n;
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
-        fin >> a[i][j];
+            fin >> a[i][j];
 }
-
-void afisare (int a[105][105], int &n)
+int PrimaCifra (int n)
 {
-    ofstream fout ("matrice.out");
-
+    while(n > 9)
+        n /= 10;
+    return n;
 }
-
+int VerifPrimaCifPara(int n)
+{
+    if(PrimaCifra(n) % 2 == 0)
+        return 1;
+    return 0;
+}
+int minimul(int c)
+{
+    int mini = 200000000;
+    for(int i = c + 1; i < n; i++)
+        if(VerifPrimaCifPara(a[i][c]) == 1 && a[i][c] < mini)
+            mini = a[i][c];
+    return mini;
+}
+void afisare()
+{
+    for(int j = 0; j < n - 1; j++)
+    {
+        int v = minimul(j);
+        if(v == 200000000)
+            cout << "nu exista";
+        else
+            cout << v;
+        cout << endl;
+    }
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+    citire();
+    afisare();
     return 0;
 }
